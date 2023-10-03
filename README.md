16983d9f8d55b49e50d9b5f865ef174d1bd916c5
Alex Mastin <alexander.mastin@cvshealth.com>
2023-10-03 09:55:11 -0500

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# eslint-config-cvs
+
+> A reusable configuration for ESLint
+
+This package supplies a ESLint configuration to the entire monorepo.
+By reusing the same configuration across all projects, it ensures that best practices are followed and that the code is consistent.
+
+For more information on ESLint, see the [docs](https://eslint.org/docs/latest/).
---
+++ index.js
@@ -0,0 +1,38 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: ['turbo', 'prettier', 'plugin:@typescript-eslint/recommended'],
+  overrides: [
+    {
+      extends: [
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
+      ],
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: path.join(__dirname, 'tsconfig.json')
+      }
+    }
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: path.join(__dirname, 'tsconfig.json')
+  },
+  plugins: ['@typescript-eslint', 'unused-imports'],
+  rules: {
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports'
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'unused-imports/no-unused-imports': 'warn',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      { checksVoidReturn: { attributes: false } }
+    ]
+  }
+}
